46921e44aeedc4e4d5e5f7937b5145bcaba28c8e
Ashwinie Parameswaran <Ashwinie.Parameswaran@siriuscom.com>
2017-08-08 15:43:21 +0530

    Folder structure

---
+++ src/js/screens/homeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../css/homeScreen.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "wrapper__homeWrapper">
+      </div>
+    );
+  }
+}
+
+export default App;

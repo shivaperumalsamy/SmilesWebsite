cd515b68c352cb71951e0556e55477cd4a5a7939
Ashwinie Parameswaran <Ashwinie.Parameswaran@siriuscom.com>
2017-12-10 18:29:40 +0530

    css added

--- src/js/screens/homeScreen.js
+++ src/js/screens/homeScreen.js
@@ -17,34 +17,14 @@ import TimelineScreen from './TimelineScreen';
   */
 }
 class HomeScreen extends Component {
-  
-  state = {
-        contentObj : {
-          headerContent : null,
-          mainContent : null
-        } 
-      }  
-  /* 
-      Service call to fetch the content 
-  */
-  componentWillMount = () => {
-    this.setState ({
-      contentObj : {
-        headerContent : "lorem ipsum",
-        mainContent : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
-      }
-    });
-   } 
-// content bar from section 1 is removed, it is ain't supposed to be there. i.e is about screen   
-//<ContentBar  contentObj={this.state.contentObj}/>
   render() {
     return (  
     <div>	
        <MenuBar/>
-       {/* <ScrollableAnchor id={commonObj.sectionIds[0]}>
+       <ScrollableAnchor id={commonObj.sectionIds[0]}>
           <Carousel active="1" className="height-100vh">
           </Carousel>
-       </ScrollableAnchor> */}
+       </ScrollableAnchor>
        <ScrollableAnchor id={commonObj.sectionIds[1]}>
           <div className="height-100vh">
             <AboutScreen/>
